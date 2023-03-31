b4da464696a7b98dd09797a9994ffe6efdd1c981
SeungEun Baek <sebaek@gmail.com>
2023-03-31 15:53:41 +0900

    bean

---
+++ src/main/java/com/study/ch05/bean/Bean02.java
@@ -0,0 +1,32 @@
+package com.study.ch05.bean;
+
+public class Bean02 {
+	private int id;
+	private String model;
+	private double price;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+}
